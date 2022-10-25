99ba6172b7b901490a6e7db9df997fc52a2ebee9
codetributor <okaclint@gmail.com>
2022-10-25 11:14:52 -1000

    new name

--- components/Logo.js
+++ components/Logo.js
@@ -1,10 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Logo() {
   return (
     <div
-    className="text-white font-extrabold"
-    >Zamps Business Client</div>
+    className="text-white font-extrabold flex justify-center items-center"
+    >
+      <Image src="/MikeLogo.png" height={50} width={50} />
+      <h1
+      className='ml-2 uppercase text-2xl'
+      >Mike's Shoes</h1>
+    </div>
   )
 }
 
